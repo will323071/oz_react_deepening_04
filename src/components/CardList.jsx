929767da56916c9c2d1189e6fc811ed65895f963
will323071 <cylee323071@gmail.com>
2025-08-08 05:00:15 +0000

    스테이징한 변경사항을 커밋한다

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,7 +1,7 @@
 import Card from './Card';
 
-export default function CardList({ data: listData }) {
-    console.log(listData);
+export default function CardList({data: listData}) {
+    console.log('listData', listData);
     //1. listData를 받아와서 카드 리스트를 렌더링 해야 합니다.
     //2. listData의 각 요소를 Card 컴포넌트에 props로 넘겨주어야 합니다.
     //3. Card 컴포넌트에는 item을 넘겨주어야 합니다.
@@ -9,7 +9,9 @@ export default function CardList({ data: listData }) {
     //5. Card 컴포넌트를 map을 이용해 여러번 렌더링할 때, Card에 key를 설정해야합니다.
     return (
         <div className="card-grid">
-            <Card />
+            {listData.map((item) => (
+                <Card key= {item.id} item= {item}/>
+            ))}
         </div>
     );
-}
+};
